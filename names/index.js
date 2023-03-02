37bf257921dc973c812e4daab8fee3def5d3554e
akhila chithanoori <cakhila982@gmail.com>
2023-03-02 15:20:12 +0000

    used helper utility function

--- names/index.js
+++ names/index.js
@@ -1,11 +1,19 @@
 const names = require("../country/state/city/index");
+const firstNamesList = require("../utilities/utils/index");
 const getPeopleInCity = (nameList) => {
-  const firstNames = [];
-  names.forEach((obj) => {
-    let { firstName, lastName } = obj;
-    firstNames.push(firstName);
-  });
+  const firstNames = firstNamesList(names);
+  console.log(firstNames);
   return firstNames;
 };
+
 getPeopleInCity(names);
 module.exports = getPeopleInCity;
+
+// const getPeopleInCity = (nameList) => {
+//   const firstNames = [];
+//   names.forEach((obj) => {
+//     let { firstName, lastName } = obj;
+//     firstNames.push(firstName);
+//   });
+//   return firstNames;
+// };
